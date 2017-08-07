defddb739190e984fb61e67a239f039916b5c8c2
Dmitry Akentyev <TogusaRusso@gmail.com>
2017-08-07 13:25:31 +0000

    Heroku database

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-  url: `mongodb://localhost:27017/passport`
+    url: process.env.MONGOLAB_URI || process.env.MONGODB_URI || `mongodb://localhost:27017/passport`
 
 }
